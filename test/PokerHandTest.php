727826cf4ec71a4708515ae604f8d161e32af799
Adam Hundley <adamhundley@gmail.com>
2018-11-08 21:32:08 -0700

    add tests to cover all hands

--- test/PokerHandTest.php
+++ test/PokerHandTest.php
@@ -17,6 +17,24 @@ class PokerHandTest extends TestCase
     /**
      * @test
      */
+    public function itCanRankAStraightFlush()
+    {
+        $hand = new PokerHand('Ks Qs Js 10s 9s');
+        $this->assertEquals('Straight Flush', $hand->getRank());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanRankAStraight()
+    {
+        $hand = new PokerHand('Ks Qc Jh 10s 9h');
+        $this->assertEquals('Straight', $hand->getRank());
+    }
+
+    /**
+     * @test
+     */
     public function itCanRankAPair()
     {
         $hand = new PokerHand('Ah As 10c 7d 6s');
@@ -35,11 +53,45 @@ class PokerHandTest extends TestCase
     /**
      * @test
      */
+    public function itCanRankThreeOfAKind()
+    {
+        $hand = new PokerHand('Kh Kc Ks 3h 2d');
+        $this->assertEquals('Three of a Kind', $hand->getRank());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanRankFourOfAKind()
+    {
+        $hand = new PokerHand('Kh Kc Ks Kd 2d');
+        $this->assertEquals('Four of a Kind', $hand->getRank());
+    }
+
+    /**
+     * @test
+     */
+    public function itCanRankFullHouse()
+    {
+        $hand = new PokerHand('Kh Kc Ks 3h 3d');
+        $this->assertEquals('Full House', $hand->getRank());
+    }
+
+    /**
+     * @test
+     */
     public function itCanRankAFlush()
     {
         $hand = new PokerHand('Kh Qh 6h 2h 9h');
         $this->assertEquals('Flush', $hand->getRank());
     }
 
-    // TODO: More tests go here
+    /**
+     * @test
+     */
+    public function itCanRankHighCard()
+    {
+        $hand = new PokerHand('Kh Qs 6h 2h 9h');
+        $this->assertEquals('High Card', $hand->getRank());
+    }
 }
